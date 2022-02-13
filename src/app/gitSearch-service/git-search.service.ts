bfd6237b68847821f60e9d66f1982291ee48f1cb
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-13 20:25:34 +0300

    Initializing the service

--- src/app/gitSearch-service/git-search.service.ts
+++ src/app/gitSearch-service/git-search.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../user-class/user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitSearchService {
 
-  constructor() { }
+  user!: User
+
+  constructor(private http:HttpClient) {
+    this.user = new User('')
+  }
 }
