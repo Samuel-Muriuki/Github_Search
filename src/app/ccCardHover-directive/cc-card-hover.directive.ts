1d4c7433f1b6d72f81e52f7c6a9b2a3021c1f5a1
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-14 03:21:38 +0300

    Implementing the custom directive on card hover

--- src/app/ccCardHover-directive/cc-card-hover.directive.ts
+++ src/app/ccCardHover-directive/cc-card-hover.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCcCardHover]'
 })
 export class CcCardHoverDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
+  @HostListener('mouseenter')onMouseEnter() {
+    this.CcCardHover('gray');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.CcCardHover('white');
+  }
+
+  private CcCardHover(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 }
